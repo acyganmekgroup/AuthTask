83fbabf51fc583e1e879892d35d3f746ed83c542
acyganmekgroup <60469000+acyganmekgroup@users.noreply.github.com>
2020-01-30 12:11:32 +0100

    Update AppData.java

--- app/src/main/java/finals/AppData.java
+++ app/src/main/java/finals/AppData.java
@@ -7,10 +7,10 @@ package finals;
  */
 public class AppData {
     // manager data
-    public static final String managerEmail = "sowaya6517@repshop.net"; // proszę wpisać wygenerowny email. (strona: https://temp-mail.org/pl/)
+    public static final String managerEmail = "acygan@mekgroup.pl"; // proszę wpisać wygenerowny email. (strona: https://temp-mail.org/pl/)
     public static final String managerPassword = "manager";
 
     // kelner data
-    public static final String kelnerEmail = "kuspupirke@enayu.com"; // proszę wpisać wygenerowny email. (strona: https://tempail.com/pl/)
+    public static final String kelnerEmail = "mlaskowski@odkupieniewin.pl"; // proszę wpisać wygenerowny email. (strona: https://tempail.com/pl/)
     public static final String kelnerPassword = "kelner";
 }
